941678ef22c96e489a5223ed4766aa12a0c1f60a
Mateus Oliveira <mateusteknus@gmail.com>
2018-04-19 13:08:40 -0300

    player ok

---
+++ server/server.go
@@ -0,0 +1,49 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+// 	file, err := os.Open("output.mp3")
+// 	if err != nil {
+// 		fmt.Println("Errou")
+// 		return
+// 	}
+// 	f := bufio.NewReader(file)
+// 	_, _ = f.Read()
+// }
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Open("soldier.mp3")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	data := make([]byte, 1024*16)
+	for {
+		_, err = f.Read(data)
+		charData := string(data) + "\n"
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(charData[:len(charData)-2])
+		time.Sleep(1)
+	}
+}
