2c15133fb99228e07fc8362579509a84bc250526
Mateus Oliveira <mateusteknus@gmail.com>
2018-04-19 13:33:11 -0300

    build script

--- bintestes/build.sh
+++ bintestes/build.sh
@@ -1,3 +1,3 @@
-go build radio/server/server.go
-go build radio/client_listenner/cliente_listenner.go
-go build radio/client_control/client_control.go
+go build ../server/server.go
+go build ../client_listenner/cliente_listenner.go
+go build ../client_control/client_control.go
