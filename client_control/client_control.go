f9e19c0a773641070cfe939d7d54f1a89588e36f
Mateus Oliveira <mateusteknus@gmail.com>
2018-04-22 17:40:43 -0300

    client control wit tcp sockect and simple message comunication

--- client_control/client_control.go
+++ client_control/client_control.go
@@ -1,6 +1,46 @@
-package main;
-import "fmt"
+package main
 
-func main(){
-    fmt.Println("Hello")
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func readShell(toControl chan<- string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Shell")
+	for {
+		text, _ := reader.ReadString('\n')
+		toControl <- text
+	}
+	close(toControl)
+}
+
+func readFromConn(fromControl chan string, conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		text, err := reader.ReadString('\n')
+		if err == nil {
+			fromControl <- text
+		}
+	}
+}
+
+func main() {
+	keyBoardInput := make(chan string)
+	fromServer := make(chan string)
+	conn, err := net.Dial("tcp", "localhost:"+os.Args[1])
+	go readShell(keyBoardInput)
+	go readFromConn(fromServer, conn)
+	if err == nil {
+		for {
+			select {
+			case msg := <-keyBoardInput:
+				conn.Write([]byte(msg))
+			case msg := <-fromServer:
+				fmt.Println(msg)
+			}
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"os"
 	"time"
 )
 
-func main() {
+const buffSize = 1024 * 16
+
+func loadMusic(name string, stationChan chan []byte) {
+	f, err := os.Open(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	data := make([]byte, buffSize)
+	for {
+		for {
+			_, err = f.Read(data)
+			if err != nil {
+				if err == io.EOF {
+					fmt.Printf("Restart Music %s\n", name)
+					break
+				}
+				fmt.Println(err)
+				return
+			}
+			stationChan <- data
+		}
+	}
+
+}
+
+func playLocal() {
 	f, err := os.Open("output.mp3")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer f.Close()
-	data := make([]byte, 1024*16)
+	data := make([]byte, buffSize)
 	for {
 		_, err = f.Read(data)
 		charData := string(data) + "\n" //teste para o delimitador no socket
@@ -26,6 +56,74 @@ func main() {
 			return
 		}
 		fmt.Println(charData[:len(charData)-2])
-		time.Sleep(3)
+		time.Sleep(2)
+	}
+}
+
+func main() {
+	server := &Server{}
+	server.StartServer(os.Args)
+	server.AcceptConn()
+}
+
+type Station struct {
+	clientList []*Client
+	newClient  chan net.Conn
+}
+
+type Client struct {
+	conn     net.Conn
+	udpPort  string
+	stations []*Station
+}
+
+func (c *Client) ListenConn() {
+	reader := bufio.NewReader(c.conn)
+	for {
+		command, err := reader.ReadString('\n')
+		if err == nil {
+			c.conn.Write([]byte(command + "@"))
+			if err != nil {
+				fmt.Println("No client connection")
+			}
+		}
+	}
+}
+
+type Server struct {
+	ln            net.Listener
+	toclientsList chan net.Conn
+	stations      []Station
+}
+
+func (server *Server) StartServer(arg []string) {
+	ln, err := net.Listen("tcp", ":"+arg[1])
+	if err != nil {
+		log.Println(err)
+	}
+	server.ln = ln
+	server.toclientsList = make(chan net.Conn)
+}
+
+func (server *Server) AcceptConn() {
+	go server.HandleClients()
+	for {
+		conn, err := server.ln.Accept()
+		if err == nil || conn != nil {
+			server.toclientsList <- conn
+		}
+	}
+}
+
+func (server *Server) HandleClients() {
+	for {
+		select {
+		case newConn := <-server.toclientsList:
+			client := &Client{conn: newConn}
+			go client.ListenConn()
+			fmt.Println("Newclinte", client.udpPort)
+		default:
+			continue
+		}
 	}
 }
