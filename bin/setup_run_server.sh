77dc587d6ae27487d05c4fd386fd0308fa04899a
Mateus Oliveira <devmateusoliveira@gmail.com>
2019-10-05 17:24:52 -0300

    Fixing setup

--- bin/setup_run_server.sh
+++ bin/setup_run_server.sh
@@ -1,4 +1,4 @@
 
-go install github.com/teknus/Radio/format_msg
-go install github.com/teknus/Radio/server
+go install github.com/teknus/radio/format_msg
+go install github.com/teknus/radio/server
 go run src/run_server.go 1234 music/Afterlife128.mp3 music/still_waiting_128.mp3
